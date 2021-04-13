688850ebdce872d89850c8fb68fdcee78a19a1ee
wale soyinka <wsoyinka@gmail.com>
2021-04-13 14:45:35 -0400

    Update build_package.sh
    fix script name and enforce providing branch name.

--- build_package.sh
+++ build_package.sh
@@ -3,12 +3,14 @@ NAME="$1"
 BRANCH="$2"
 
 if [[ -z "$NAME" ]]; then
-    echo "usage: import_package.sh NAME"
+    echo "usage: build_package.sh NAME BRANCH_NAME"
+    echo "usage e.g. :  build_package.sh  nginx r8-stream-1.18"
     exit 1
 fi
-if [[ -z "$BRANCH" ]]; then
-    BRANCH="r8"
-fi
+
+#if [[ -z "$BRANCH" ]]; then
+#    BRANCH="r8"
+#fi
 
 cd "rpms/$NAME/$BRANCH"
 
